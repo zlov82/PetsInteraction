77b8fce39a84ecb54ff528441720d2af146cf2db
aleksandr <dev.golovkoay@gmail.com>
2024-02-07 14:03:10 +0300

    initial

---
+++ src/main/java/ru/yandex/practicum/controllers/CatsInteractionController.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cats")
+public class CatsInteractionController {
+    private int happiness = 0;
+
+    @GetMapping("/converse")
+    public Map<String, String> converse() {
+        happiness++;
+        return Map.of("talk", "Мяу");
+    }
+
+    @GetMapping("/pet")
+    public Map<String, String> pet(@RequestParam(required = false) final Integer count) {
+        happiness += count;
+        return Map.of("talk", "Муррр. ".repeat(count));
+    }
+
+    @GetMapping("/happiness")
+    public Map<String, Integer> happiness() {
+        return Map.of("happiness", happiness);
+    }
+}
